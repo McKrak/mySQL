b900f84babff9069ec9cb1399a59718e6eb8a911
alextubbaa <alextubba@example.com>
2023-02-24 08:55:37 -0500

    Add connection to SQL Server

--- mySQL/mySQL/InputPage.xaml.cs
+++ mySQL/mySQL/InputPage.xaml.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using System.Windows;
+
 namespace mySQL;
 
 public partial class InputPage : ContentPage
@@ -6,4 +9,20 @@ public partial class InputPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void OnButtonClicked(object sender, EventArgs args)
+	{
+        // get connection
+        string connectionString = "server=192.169.144.133;database=sr_team_6;uid=mcctc6;pwd=mcctcrocks;";
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();
+
+        // publish data
+        string query = "INSERT INTO mytable (column1, column2) VALUES ('value1', 'value2')";
+        MySqlCommand command = new MySqlCommand(query, connection);
+        command.ExecuteNonQuery();
+
+        // close
+        connection.Close();
+    }
 }
